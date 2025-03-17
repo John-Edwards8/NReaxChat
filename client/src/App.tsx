ce3bfaa75fce1b4a1f7ec563ca01ee3c1ca0e05b
John Edwards <86607566+John-Edwards8@users.noreply.github.com>
2025-03-17 20:50:25 +0100

    ChatRoom separated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,10 @@
-//import { useState } from 'react'
 import './App.css'
+import ChatRoom from './ChatRoom'
 
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="card">
-        <div className="chat-header">Chat</div>
-        <div className="chat-window">
-          <ul className="message-list" />
-        </div>
-        <div className="chat-input">
-          <input type="text" className="message-input" placeholder="Type your message here" />
-          <button className="send-button">Send</button>
-        </div>
-      </div>
+      <ChatRoom />
     </>
   )
 }
