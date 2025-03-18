9ff9a5dd3738a5bb9c934d04cf2731e572bfb92a
John Edwards <86607566+John-Edwards8@users.noreply.github.com>
2025-03-18 01:01:22 +0100

    Sending messages is possible
    Websocket configured

--- client/src/ChatRoom.tsx
+++ client/src/ChatRoom.tsx
@@ -1,17 +1,79 @@
+import { useEffect, useState, useRef } from "react";
 import './ChatRoom.css'
+import HandleInput from "./HandleInput.tsx";
+import { Client } from '@stomp/stompjs';
+import Messages from "./Messages.tsx";
 
 function ChatRoom() {
+  let [message, setMessage] = useState('');
+  let [messages, setMessages] = useState<string[]>([]); 
+  const stompClientRef = useRef<Client>(null);
+
+  useEffect(() => {
+    const stompClient = new Client({
+        webSocketFactory: () => new WebSocket('ws://localhost:8080/chat'),
+        reconnectDelay: 5000,
+        debug: (str) => {
+            console.log("DEBUG: ", str);
+        },
+        onConnect: () => {
+            console.log('Connected to WebSocket');
+            stompClient.subscribe('/topic/messages', (response) => {
+                console.log('Received message:', response.body);
+                const data = JSON.parse(response.body);
+                if (data.sender && data.content) {
+                  setMessages((prevMessages) => [...prevMessages, `${data.sender}: ${data.content}`]);
+                }
+            });
+        },
+        onStompError: (frame) => {
+            console.error('Broker reported error: ' + frame.headers['message']);
+            console.error('Additional details: ' + frame.body);
+        },
+    });
+
+    stompClient.activate();
+    stompClientRef.current = stompClient;
+
+    return () => {
+      stompClient.deactivate();
+    };
+}, []);
+
+  const sendMessage = () => {
+    console.log("📤 Отправка сообщения:", message);
+    const stompClient = stompClientRef.current;
+    if (stompClient && stompClient.connected) {
+      stompClient.publish({
+        destination: "/app/sendMessage",
+        body: JSON.stringify({
+          senderName: "User1",
+          content: message
+        }),
+      });
+      setMessage('');
+    } else {
+        console.error('Stomp client is not connected');
+    }
+  }
+
   return (
     <>
       <div className="card">
         <div className="chat-header">Chat</div>
         <div className="chat-window">
-          <ul className="message-list" />
+          <Messages 
+            messages={messages}
+          />
         </div>
         <div className="chat-input">
-          <input type="text" className="message-input" placeholder="Type your message here" />
-          <button className="send-button">Send</button>
+          <HandleInput
+            message={message}
+            setMessage={setMessage}
+          />
+          <button className="send-button" onClick={sendMessage}>Send</button>
         </div>
+        
       </div>
     </>
   )
