97cef39a1691ce8647e62e4a2b09b1d421de7889
John Edwards <86607566+John-Edwards8@users.noreply.github.com>
2025-03-24 00:24:20 +0100

    AuthServer initialized
    Optimization of structure
    JDK updated (UNTESTED)

---
+++ backend/AuthServer/src/main/java/com/john/protoAuth/ProtoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.john.protoAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoAuthApplication.class, args);
+	}
+
+}
