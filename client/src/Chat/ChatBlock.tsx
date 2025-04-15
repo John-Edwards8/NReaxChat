e298631ccbfda240b03deb56681bd9ee41ffcc14
NotStancho <jadanstanislav05@gmail.com>
2025-04-15 21:15:23 +0300

    Backgroud changed

--- client/src/Chat/ChatBlock.tsx
+++ client/src/Chat/ChatBlock.tsx
@@ -5,9 +5,11 @@ function ChatBlock() {
     <>
       <div className="card-cc">
         <div className="chats-header">Chats</div>
-        <div className="chats-window">
-          <ul className="chats-list" />
-        </div>
+          {/*<div className="chats-body">*/}
+            <div className="chats-window">
+                <ul className="chats-list" />
+            </div>
+          {/*</div>*/}
       </div>
     </>
   )
