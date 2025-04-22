b4cc057c575573754a1c63cb824173d812d06538
Maksym <chernenkomax04@gmail.com>
2025-04-22 22:07:19 +0200

    NREAX-10 Update router: add register and login endpoints, remove createUser endpoint

--- backend/AuthServer/src/main/java/com/john/auth/AuthRouter.java
+++ backend/AuthServer/src/main/java/com/john/auth/AuthRouter.java
@@ -7,19 +7,18 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration(proxyBeanMethods = false)
 public class AuthRouter {
-    @Bean
-    RouterFunction<ServerResponse> route(AuthHandler handler) {
-    	return RouterFunctions
-			.route(GET("/token").and(accept(MediaType.APPLICATION_JSON)), handler::hello)
-			.andRoute(GET("/user/{username}").and(accept(MediaType.APPLICATION_JSON)), handler::getUser)
-			.andRoute(GET("/createUser").and(accept(MediaType.APPLICATION_JSON)), handler::createUser)
-    		.andRoute(GET("/deleteUser/{del}").and(accept(MediaType.APPLICATION_JSON)), handler::deleteUser)
-    		.andRoute(GET("/updateUser/{upd}").and(accept(MediaType.APPLICATION_JSON)), handler::updateUser);
+
+	@Bean
+	public RouterFunction<ServerResponse> route(AuthHandler handler) {
+		return RouterFunctions
+				.route(POST("/register").and(accept(MediaType.APPLICATION_JSON)), handler::register)
+				.andRoute(POST("/login").and(accept(MediaType.APPLICATION_JSON)), handler::login)
+				.andRoute(GET("/user/{username}").and(accept(MediaType.APPLICATION_JSON)), handler::getUser)
+				.andRoute(PUT("/updateUser/{username}").and(accept(MediaType.APPLICATION_JSON)), handler::updateUser)
+				.andRoute(DELETE("/deleteUser/{username}").and(accept(MediaType.APPLICATION_JSON)), handler::deleteUser);
 	}
-	
 }
