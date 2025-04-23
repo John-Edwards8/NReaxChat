4fedb2b11e9d7582355238b9b69a9efed3762f06
Ankana34 <anna.huzenko@nure.ua>
2025-04-23 23:58:55 +0200

    NREAX-1 Preparing for mongodb

--- backend/ChatServer/src/main/java/com/john/protoChat/repository/MessageRepository.java
+++ backend/ChatServer/src/main/java/com/john/protoChat/repository/MessageRepository.java
@@ -1,7 +1,7 @@
 package com.john.protoChat.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import com.john.protoChat.model.Message;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {
+public interface MessageRepository extends ReactiveMongoRepository<Message, Long> {
 }
