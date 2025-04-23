64876e4c2353fdf2180c4776cfb18d5d9e95c2a8
NotStancho <jadanstanislav05@gmail.com>
2025-04-23 18:28:08 +0300

    NREAX-20 Implemented right sidebar

---
+++ client/src/features/chat/SidebarRight/ChatSidebarRight.tsx
@@ -0,0 +1,15 @@
+import AppLogo from './AppLogo.tsx'
+import Settings from './Settings.tsx'
+
+function ChatSidebarRight() {
+    return (
+        <div className="w-[320px] rounded-22 bg-[#0F172A]/50 shadow-[0_4px_12px_rgba(0,0,0,0.5)] flex flex-col m-2 overflow-hidden">
+            <AppLogo/>
+            <hr className="border-gray-300/30 m-2" />
+            <Settings/>
+            <hr className="border-gray-300/30 m-2" />
+        </div>
+    );
+}
+
+export default ChatSidebarRight;
