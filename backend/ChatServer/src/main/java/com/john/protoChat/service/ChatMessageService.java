84e6cdac2b8c6671f0ff89c9a810d00e4b8c4aa8
Ankana34 <anna.huzenko@nure.ua>
2025-04-25 13:33:48 +0200

    NREAX-2 Implement handler and routing logic

--- backend/ChatServer/src/main/java/com/john/protoChat/service/ChatMessageService.java
+++ backend/ChatServer/src/main/java/com/john/protoChat/service/ChatMessageService.java
@@ -1,29 +1,30 @@
 package com.john.protoChat.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.john.protoChat.model.ChatMessage;
 import com.john.protoChat.model.Message;
 import com.john.protoChat.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ChatMessageService {
-	
-    @Autowired private MessageRepository messageRepository;
-    
-    public List<Message> getMessages() {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Flux<Message> getMessages() {
         return messageRepository.findAll();
     }
-	
-    public ChatMessage send(ChatMessage message) {
+
+    public Mono<ChatMessage> send(ChatMessage message) {
         Message savedMessage = new Message();
         savedMessage.setSender(message.getSenderName());
         savedMessage.setContent(message.getContent());
-        messageRepository.save(savedMessage);
-        
-        return message;
+        savedMessage.setTimestamp(message.getTimestamp());
+
+        return messageRepository.save(savedMessage)
+                .thenReturn(message);
     }
 }
