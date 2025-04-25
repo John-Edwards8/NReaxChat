fb8f191b0f361994a73a89249628b4383adf2e02
NotStancho <jadanstanislav05@gmail.com>
2025-04-25 12:11:35 +0300

    Old logic saved

---
+++ client/src/components/Messages.tsx
@@ -0,0 +1,10 @@
+const Messages = ({messages} : {messages:string[]}) => {
+    return(
+        <ul className="message-list">
+            {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+            ))}
+        </ul>
+    )
+  }
+  export default Messages
