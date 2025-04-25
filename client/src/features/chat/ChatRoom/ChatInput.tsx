9bc7395d69b91923d7bc6834fc38e64fb9b6d3da
John-Edwards8 <ponomarevserg007@gmail.com>
2025-04-25 22:33:35 +0200

    Returned WebSocket on front

--- client/src/features/chat/ChatRoom/ChatInput.tsx
+++ client/src/features/chat/ChatRoom/ChatInput.tsx
@@ -1,15 +1,33 @@
 import {FaPaperPlane} from "react-icons/fa";
 
-const ChatInput = () => {
+type HandleInputProps = {
+    message: string;
+    setMessage: React.Dispatch<React.SetStateAction<string>>;
+    sendMessage: () => void;
+};
+
+
+const ChatInput = ({ message, setMessage, sendMessage }: HandleInputProps) => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+          sendMessage();
+        }
+      };
     return (
         <div className="flex items-center bg-blue-base px-4 py-2">
             <input
                 type="text"
-                placeholder="Type a message..."
+                value={message}
                 className="flex-1 bg-transparent outline-none placeholder-white"
-                //className="flex-1 bg-transparent outline-none"
+                placeholder="Type a message..."
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setMessage(e.target.value)}
             />
-            <button type="button" className="hover:opacity-80 transition">
+            <button
+                type="button"
+                className="hover:opacity-80 transition"
+                onClick={sendMessage}
+            >
                 <FaPaperPlane className="w-8 h-8"/>
             </button>
         </div>
--- client/src/features/chat/ChatRoom/ChatRoom.tsx
+++ client/src/features/chat/ChatRoom/ChatRoom.tsx
@@ -1,13 +1,83 @@
 import ChatHeader from "./ChatHeader.tsx";
 import MessagesList from "./MessagesList.tsx";
 import ChatInput from "./ChatInput.tsx";
+import { useState, useEffect, useRef } from 'react';
+import { Client } from '@stomp/stompjs';
 
 function ChatRoom() {
+    type Message = {
+      text: string;
+      sender: 'me' | 'other';
+    };
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState<Message[]>([]); 
+    const stompClientRef = useRef<Client>(null);
+
+    useEffect(() => {
+        const stompClient = new Client({
+            webSocketFactory: () => new WebSocket('ws://localhost:8080/chat'),
+            reconnectDelay: 5000,
+            debug: (str) => {
+                console.log("DEBUG: ", str);
+            },
+            onConnect: () => {
+                console.log('Connected to WebSocket');
+                stompClient.subscribe('/topic/messages', (response) => {
+                    console.log('Received message:', response.body);
+                    const data = JSON.parse(response.body);
+                    if (data.sender && data.content) {
+                      setMessages((prevMessages) => [
+                        ...prevMessages,
+                        {
+                          text: data.content,
+                          sender: data.sender === 'User1' ? 'me' : 'other'
+                        }
+                      ]);
+                    }
+                });
+            },
+            onStompError: (frame) => {
+                console.error('Broker reported error: ' + frame.headers['message']);
+                console.error('Additional details: ' + frame.body);
+            },
+        });
+
+        stompClient.activate();
+        stompClientRef.current = stompClient;
+
+        return () => {
+          stompClient.deactivate();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        console.log("📤 Отправка сообщения:", message);
+        const stompClient = stompClientRef.current;
+        if (stompClient && stompClient.connected) {
+          stompClient.publish({
+            destination: "/app/sendMessage",
+            body: JSON.stringify({
+              senderName: "User1",
+              content: message
+            }),
+          });
+          setMessage('');
+        } else {
+          console.error('Stomp client is not connected');
+        }
+    }
+
     return(
         <div className="flex flex-col flex-1 rounded-22 bg-[#0F172A]/50 shadow-[0_4px_12px_rgba(0,0,0,0.5)] m-2 overflow-hidden">
             <ChatHeader/>
-            <MessagesList/>
-            <ChatInput/>
+            <MessagesList
+                messages={messages}
+            />
+            <ChatInput
+                message={message}
+                setMessage={setMessage}
+                sendMessage={sendMessage}
+            />
         </div>
     );
 }
