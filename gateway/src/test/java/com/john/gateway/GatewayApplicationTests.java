43ca87a2b4ff5c884405ce341d99e5b04b9916fd
Maksym <chernenkomax04@gmail.com>
2025-05-06 20:34:35 +0200

    NREAX-44 Write Gateway tests

--- gateway/src/test/java/com/john/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/john/gateway/GatewayApplicationTests.java
@@ -3,7 +3,12 @@ package com.john.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"AUTH_URL=http://auth-service.test",
+		"CHAT_URL=http://chat-service.test",
+		"FRONTEND_URL=http://frontend.test",
+		"FRONTEND_URL_DEV=http://frontend-dev.test"
+})
 class GatewayApplicationTests {
 
 	@Test
