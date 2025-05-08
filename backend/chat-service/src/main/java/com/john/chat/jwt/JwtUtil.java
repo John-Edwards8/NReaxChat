813e5751a6558dc6056ce430d9dadbed501af174
Maksym <chernenkomax04@gmail.com>
2025-05-08 08:07:45 +0200

    NREAX-46 Add JWT logic to chat-service

---
+++ backend/chat-service/src/main/java/com/john/chat/jwt/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.john.chat.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@Setter
+@SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "Used safely")
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String base64Secret;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(base64Secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims body = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            Date expiration = body.getExpiration();
+
+            return !expiration.before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
