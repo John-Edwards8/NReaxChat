d65a56c208b559f177a654698070444a3864ddb5
John-Edwards8 <ponomarevserg007@gmail.com>
2025-05-29 23:32:29 +0200

    NREAX-59 Added errors messages

---
+++ client/src/constants/errors.ts
@@ -0,0 +1,12 @@
+export const errors = {
+    missingFields: "Please enter username and password",
+    missingUsername: "Please enter username",
+    missingPassword: "Please enter password",
+    login: {
+      invalid: "Invalid credentials",
+    },
+    register: {
+      invalid: "Failed to register",
+    },
+  };
+  
