5ca27df9d18fc0e80295ac0497fb1dfbc6913477
John-Edwards8 <ponomarevserg007@gmail.com>
2025-05-30 16:34:40 +0200

    Added search (SidebarLeft)

--- client/src/components/chat/SidebarLeft/ChatSidebarLeft.tsx
+++ client/src/components/chat/SidebarLeft/ChatSidebarLeft.tsx
@@ -6,12 +6,13 @@ import { ChatTab} from "../../../types/ChatTabs";
 
 function ChatSidebarLeft() {
     const [activeTab, setActiveTab] = useState<ChatTab>('all');
+    const [search, setSearch] = useState('');
 
     return (
         <div className="w-[420px] rounded-22 bg-[#0F172A]/50 shadow-[0_4px_12px_rgba(0,0,0,0.5)] flex flex-col m-2 overflow-hidden">
-            <ChatSearchBar/>
+            <ChatSearchBar search={search} setSearch={setSearch} />
             <ChatSectionHeader activeTab={activeTab} setActiveTab={setActiveTab}/>
-            <ChatListItem activeTab={activeTab}/>
+            <ChatListItem activeTab={activeTab} search={search} />
         </div>
     );
 }
