b16bb754d8403f7909a3b7c687c1895db6fa4982
Maksym <chernenkomax04@gmail.com>
2025-06-05 18:42:41 +0200

    NREAX-84 Update Exit button logic to open confirmation modal

--- client/src/components/chat/SidebarRight/Exit.tsx
+++ client/src/components/chat/SidebarRight/Exit.tsx
@@ -1,31 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { FiLogOut } from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
-import {useAuthStore} from "../../../stores/authStore.ts";
-import api from "../../../api/axios.ts";
+import ConfirmLogoutModal from "../../modals/ConfirmLogoutModal.tsx";
 
 const Exit: React.FC = () => {
-    const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleLogout = async () => {
-        try {
-            await api.post("/auth/api/logout");
-        } catch (e) {
-            console.error("Logout error:", e);
-        } finally {
-            useAuthStore.getState().clearAuth();
-            navigate("/login");
-        }
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
     };
 
     return (
-        <div
-            className="flex items-center gap-3 px-4 cursor-pointer"
-            onClick={handleLogout}
-        >
-            <FiLogOut className="w-16 h-16 text-blue-base " />
-            <span className="text-2xl font-semibold">Exit</span>
-        </div>
+        <>
+            <div
+                className="flex items-center gap-3 px-4 cursor-pointer"
+                onClick={openModal}
+            >
+                <FiLogOut className="w-16 h-16 text-blue-base "/>
+                <span className="text-2xl font-semibold">Exit</span>
+            </div>
+            <ConfirmLogoutModal isOpen={isModalOpen} onClose={closeModal}/></>
     );
 };
 
