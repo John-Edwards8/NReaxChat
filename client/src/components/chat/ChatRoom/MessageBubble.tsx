fb8fae24ac2473331feaba17065e4f4c6eb67053
John-Edwards8 <ponomarevserg007@gmail.com>
2025-06-06 00:14:57 +0200

    NREAX-85 Updated bubble
    - added time of message
    - updated sender in group chats

--- client/src/components/chat/ChatRoom/MessageBubble.tsx
+++ client/src/components/chat/ChatRoom/MessageBubble.tsx
@@ -2,22 +2,35 @@ import { Message } from "../../../types/Message";
 
 type Props = Message & { currentUser: string; isGroup: boolean };
 
-const MessageBubble = ({ text, sender, currentUser, isGroup }: Props) => {
+const MessageBubble = ({ text, sender, timestamp, currentUser, isGroup }: Props) => {
     const isMe = sender === currentUser;
-
+    
     return (
         <div className={`flex mb-2 ${isMe ? "justify-end" : "justify-start"}`}>
             <div className={`max-w-[70%] ${isMe ? "text-right" : "text-left"}`}>
-                {!isMe && isGroup && (
-                    <div className="text-xs text-gray-400 mb-1">{sender}</div>
-                )}
                 <div
                     className={`
-            px-4 py-2 rounded-22
-            ${isMe ? "bg-blue-base rounded-br-none" : "bg-gray-800 rounded-bl-none"}
-          `}
+                        px-4 py-2 rounded-22
+                        ${isMe ? "bg-blue-base rounded-br-none" : "bg-gray-800 rounded-bl-none"}
+                        flex flex-col
+                    `}
                 >
-                    {text}
+                    {!isMe && isGroup ? (
+                        <div className="flex justify-between items-center mb-1 text-xs text-gray-400">
+                            <div>{sender}</div>
+                            <div className="whitespace-nowrap ml-5">
+                                {timestamp.toLocaleString().match(/\d\d\:\d\d/i)?.[0]}
+                            </div>
+                        </div>
+                    ) : null}
+                    <div className="flex justify-between items-end gap-2">
+                        {text}
+                        {(!isGroup || isMe) && (
+                            <div className="text-gray-400 text-xs whitespace-nowrap">
+                                {timestamp.toLocaleString().match(/\d\d\:\d\d/i)?.[0]}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
