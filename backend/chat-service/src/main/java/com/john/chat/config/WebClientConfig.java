b0ab75f3e41b5b5ff24a8e0c6275cac188d4410b
NotStancho <jadanstanislav05@gmail.com>
2025-06-16 16:06:36 +0300

    Added AUTH_SERVICE_URL support for WebClient config

--- backend/chat-service/src/main/java/com/john/chat/config/WebClientConfig.java
+++ backend/chat-service/src/main/java/com/john/chat/config/WebClientConfig.java
@@ -1,15 +1,19 @@
 package com.john.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+    @Value("${auth.service.url}")
+    private String authServiceUrl;
+
     @Bean
     public WebClient authWebClient() {
         return WebClient.builder()
-                .baseUrl("http://gateway:8080/auth/api")
+                .baseUrl(authServiceUrl)
                 .build();
     }
 }
